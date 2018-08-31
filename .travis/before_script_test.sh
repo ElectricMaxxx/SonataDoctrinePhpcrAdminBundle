cbd68291718b6a30b2e682a6963db2d315d5e55f
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-08-31 03:44:24 +0200

    use verbose output to find error

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -5,5 +5,5 @@ CONSOLE=${TESTING_SCRIPTS_DIR}/console
 
 export KERNEL_CLASS="Sonata\\DoctrinePHPCRAdminBundle\\Tests\\Fixtures\\App\\Kernel"
 echo '+++ create PHPCR +++'
-${CONSOLE} doctrine:phpcr:init:dbal --drop --force
-${CONSOLE} doctrine:phpcr:repository:init
+${CONSOLE} doctrine:phpcr:init:dbal --drop --force -vvv
+${CONSOLE} doctrine:phpcr:repository:init -vvv
