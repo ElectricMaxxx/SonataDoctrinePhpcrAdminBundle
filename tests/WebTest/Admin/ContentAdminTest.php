145c024f9fe2c2738c87c68e59b7db135b50969b
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-11-08 22:26:46 +0100

    do really do it

--- tests/WebTest/Admin/ContentAdminTest.php
+++ tests/WebTest/Admin/ContentAdminTest.php
@@ -15,6 +15,7 @@ namespace Sonata\DoctrinePHPCRAdminBundle\Tests\WebTest\Admin;
 
 use Sonata\DoctrinePHPCRAdminBundle\Tests\Fixtures\App\DataFixtures\Phpcr\LoadTreeData;
 use Symfony\Cmf\Component\Testing\Functional\BaseTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class ContentAdminTest extends BaseTestCase
 {
